7ec3f71bd591a15f670f1a6d3fa9410842f0b2ec
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-06-10 23:13:49 -0400

    updating the readme file

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,93 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      attributes: ["id", "tag_name"],
+      // be sure to include its associated Product data
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"],
+        },
+      ],
+    });
+    res.json(tagData);
+  } catch (error) {
+    res.status(400).json({ message: "Server is down" });
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagID = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "tag_name"],
+      // be sure to include its associated Product data
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"],
+        },
+      ],
+    });
+    return res.json(tagID);
+  } catch (error) {
+    res.status(400).json({ message: "Server is down" });
+  }
+  
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    return res.json(newTag);
+  } catch (error) {
+    res.status(400).json({ message: "Server is down" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+  try {
+    console.log("req.params.id ===>", req.params.id);
+    const tagID = await Tag.update(
+      { tag_name: req.body.tag_name },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    console.log("tagID ====>", tagID);
+
+    return res.json(tagID);
+  } catch (error) {
+    res.status(400).json({ message: "Server is down" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    return res.json(deleteTag);
+  } catch (error) {
+    res.status(400).json({ message: "Server is down" });
+  }
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # E-Commerce-Backend
+
+# Live URL 
+
+https://sajjazaidi2015.github.io/E-Commerce-Backend/
+
+# Description
+
+This is an E-commerce backend node.js backend that involves the use of an express.js server and a SQL database. It includes RESTful APIs that has CRUD functionality. It also implemements the usage of sequelize as its ORM tool to convert data to and from the database. Because it is a backend only application, Insomnia was used in order to make requests to the APIs.
+
+# Installation
+Please make sure you have node.js installed on your machine! <br>
+
+mysql2 installation: <br>
+```shell
+npm i mysql2
+```
+sequelize: <br>
+```shell
+npm i sequelize
+```
+express installation: <br>
+```shell
+npm i express
+```
+Once you have these things installed, you must have a mysql database running on your system, replace the values for the host, database name, and password to the database in order to connect to your own database.
+
+# Demo
+
+## Demonstrate how to seed the database from the command line.
+![Run Seed the database from the command line](./video/Run%20the%20Seed%20file%20%26%20server%20through%20the%20command%20line.gif)
+
+
+## Demonstrate GET routes for all categories, all products, and all tags being tested in Insomnia
+![Demonstrate GET routes for all categories, all products, and all tags being tested in Insomnia ](./video/13-orm-homework-demo-01.gif)
+
+## Demonstrate GET routes for a single category, a single product, and a single tag being tested in Insomnia
+
+![Demonstrate GET routes for a single category, a single product, and a single tag being tested in Insomnia ](./video/13-orm-homework-demo-02.gif)
+
+## Demonstrate POST, PUT, and DELETE routes for categories, products, and tags being tested in Insomnia.
+
+![demonstrate POST, PUT, and DELETE routes for categories, products, and tags being tested in Insomnia. ](./video/13-orm-homework-demo-03.gif)
